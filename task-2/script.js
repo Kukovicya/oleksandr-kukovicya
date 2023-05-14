933bb1fb16c2ad9abeaa641b17faa8a1e656d0ff
Kukovicya <92583408+Kukovicya@users.noreply.github.com>
2023-05-14 17:50:48 +0300

    alpha version

--- task-2/script.js
+++ task-2/script.js
@@ -7,34 +7,78 @@ document.addEventListener("DOMContentLoaded", function () {
   counter.innerHTML = `Количество изображений: ${count} | ${currentDate}`;
   document.body.insertBefore(counter, document.body.firstChild);
 
-  const imageItems = document.querySelectorAll(".image-grid .image-item img");
+  const imageItems = document.querySelectorAll(".image-grid .image-item");
   const popup = document.querySelector(".popup");
   const popupImage = document.querySelector(".popup-image");
   const popupOverlay = document.querySelector(".popup-overlay");
   const closeButton = document.querySelector(".close-button");
+  const restoreButton = document.querySelector(".restore-button");
+  const deletedImagesKey = "deletedImages";
+  let deletedImages = [];
 
-  // Обработчик клика на изображение
+  // модальне вікно
   function handleImageClick(event) {
     const clickedImageSrc = event.target.src;
+    const imageItem = event.currentTarget;
+    const index = Array.from(imageItems).indexOf(imageItem);
     popupImage.src = clickedImageSrc;
     popup.classList.add("active");
+    popupImage.dataset.index = index;
   }
 
-  // Обработчик клика на кнопку закрытия
   function handleCloseClick() {
     popup.classList.remove("active");
   }
 
-  // Обработчик клика на оверлей
+  // закриття модалки
   function handleOverlayClick() {
     popup.classList.remove("active");
   }
 
-  // Назначаем обработчики событий для изображений, кнопки закрытия и оверлея
-  imageItems.forEach(function (image) {
+  // кнопка видалення
+  function handleDeleteClick(event) {
+    const imageItem = event.currentTarget.parentNode;
+    const deletedImageSrc = imageItem.querySelector("img").src;
+    const index = Array.from(imageItems).indexOf(imageItem);
+    imageItem.style.display = "none";
+    deletedImages.push(deletedImageSrc);
+    localStorage.setItem(deletedImagesKey, JSON.stringify(deletedImages));
+  }
+
+  // кнопка відновлення
+  function handleRestoreClick() {
+    deletedImages.forEach(function (deletedImageSrc) {
+      const imageItem = Array.from(imageItems).find(function (item) {
+        return item.querySelector("img").src === deletedImageSrc;
+      });
+      if (imageItem) {
+        imageItem.style.display = "block";
+      }
+    });
+    deletedImages = [];
+    localStorage.removeItem(deletedImagesKey);
+  }
+  imageItems.forEach(function (imageItem) {
+    const image = imageItem.querySelector("img");
+    const deleteButton = imageItem.querySelector(".delete-button");
     image.addEventListener("click", handleImageClick);
+    deleteButton.addEventListener("click", handleDeleteClick);
   });
 
   closeButton.addEventListener("click", handleCloseClick);
   popupOverlay.addEventListener("click", handleOverlayClick);
+  restoreButton.addEventListener("click", handleRestoreClick);
+
+  // Перевірка локалсторедж
+  if (localStorage.getItem(deletedImagesKey)) {
+    deletedImages = JSON.parse(localStorage.getItem(deletedImagesKey));
+    deletedImages.forEach(function (deletedImageSrc) {
+      const imageItem = Array.from(imageItems).find(function (item) {
+        return item.querySelector("img").src === deletedImageSrc;
+      });
+      if (imageItem) {
+        imageItem.style.display = "none";
+      }
+    });
+  }
 });
