d8f5b8ed44e90921543966242fa466a241d55065
Kukovicya <92583408+Kukovicya@users.noreply.github.com>
2023-05-14 17:28:54 +0300

    modal window

--- task-2/script.js
+++ task-2/script.js
@@ -1,7 +1,40 @@
-const images = document.querySelectorAll(".image-grid img");
-const count = images.length;
-const currentDate = new Date().toLocaleString("ru-RU");
+document.addEventListener("DOMContentLoaded", function () {
+  const images = document.querySelectorAll(".image-grid img");
+  const count = images.length;
+  const currentDate = new Date().toLocaleString("ru-RU");
 
-const counter = document.createElement("div");
-counter.innerHTML = `Количество изображений: ${count} | ${currentDate}`;
-document.body.insertBefore(counter, document.body.firstChild);
+  const counter = document.createElement("div");
+  counter.innerHTML = `Количество изображений: ${count} | ${currentDate}`;
+  document.body.insertBefore(counter, document.body.firstChild);
+
+  const imageItems = document.querySelectorAll(".image-grid .image-item img");
+  const popup = document.querySelector(".popup");
+  const popupImage = document.querySelector(".popup-image");
+  const popupOverlay = document.querySelector(".popup-overlay");
+  const closeButton = document.querySelector(".close-button");
+
+  // Обработчик клика на изображение
+  function handleImageClick(event) {
+    const clickedImageSrc = event.target.src;
+    popupImage.src = clickedImageSrc;
+    popup.classList.add("active");
+  }
+
+  // Обработчик клика на кнопку закрытия
+  function handleCloseClick() {
+    popup.classList.remove("active");
+  }
+
+  // Обработчик клика на оверлей
+  function handleOverlayClick() {
+    popup.classList.remove("active");
+  }
+
+  // Назначаем обработчики событий для изображений, кнопки закрытия и оверлея
+  imageItems.forEach(function (image) {
+    image.addEventListener("click", handleImageClick);
+  });
+
+  closeButton.addEventListener("click", handleCloseClick);
+  popupOverlay.addEventListener("click", handleOverlayClick);
+});
