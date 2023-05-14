a3886e510eebaff4dc559045a5ce673e70efa255
Kukovicya <92583408+Kukovicya@users.noreply.github.com>
2023-05-14 16:07:52 +0300

    Update script.js

--- task-2/script.js
+++ task-2/script.js
@@ -1,3 +1,7 @@
 const images = document.querySelectorAll(".image-grid img");
 const count = images.length;
 const currentDate = new Date().toLocaleString("ru-RU");
+
+const counter = document.createElement("div");
+counter.innerHTML = `Количество изображений: ${count} | ${currentDate}`;
+document.body.insertBefore(counter, document.body.firstChild);
