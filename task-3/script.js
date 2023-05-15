3da9f1ba1e4b3674698bd6816f2cab7c5792974e
Kukovicya <92583408+Kukovicya@users.noreply.github.com>
2023-05-15 14:08:37 +0300

    alpha version

--- task-3/script.js
+++ task-3/script.js
@@ -1,65 +1,101 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const countryInput = document.getElementById("country-input");
-  const searchButton = document.getElementById("search-button");
-  const resetButton = document.getElementById("reset-button");
-  const resultsTable = document
-    .getElementById("results-table")
-    .querySelector("tbody");
+$(document).ready(function () {
+  // Проверка, есть ли сохраненные данные
+  if (localStorage.getItem("universities")) {
+    $("#search-results").html(localStorage.getItem("universities"));
+    restoreCheckboxState();
+    updateCheckboxCount();
+  }
 
-  searchButton.addEventListener("click", function () {
-    const country = countryInput.value.trim().toLowerCase();
-    if (country === "") {
-      alert("Пожалуйста, введите название страны.");
-      return;
-    }
+  // Обработчик нажатия на кнопку "Отправить"
+  $("#search-form").submit(function (event) {
+    event.preventDefault();
+    var country = $("#country-input").val();
 
-    // Очищаем предыдущие результаты
-    resultsTable.innerHTML = "";
+    $.getJSON(
+      "http://universities.hipolabs.com/search?country=" + country,
+      function (data) {
+        var table = $("<table></table>");
+        var headerRow = $(
+          "<tr><th>#</th><th>Name</th><th>Website</th><th>Country</th><th>Save</th></tr>"
+        );
+        table.append(headerRow);
 
-    // Отправляем запрос к API
-    fetch(`http://universities.hipolabs.com/search?country=${country}`)
-      .then((response) => response.json())
-      .then((data) => {
-        // Проверяем, есть ли результаты
-        if (data.length === 0) {
-          alert("Не найдено университетов для данной страны.");
-          return;
-        }
-
-        // Выводим результаты в таблицу
-        data.forEach(function (university, index) {
-          const row = document.createElement("tr");
-
-          const indexCell = document.createElement("td");
-          indexCell.textContent = index + 1;
-          row.appendChild(indexCell);
-
-          const nameCell = document.createElement("td");
-          nameCell.textContent = university.name;
-          row.appendChild(nameCell);
-
-          const countryCell = document.createElement("td");
-          countryCell.textContent = university.country;
-          row.appendChild(countryCell);
-
-          const websiteCell = document.createElement("td");
-          const websiteLink = document.createElement("a");
-          websiteLink.href = university.web_pages[0];
-          websiteLink.textContent = university.web_pages[0];
-          websiteCell.appendChild(websiteLink);
-          row.appendChild(websiteCell);
-
-          resultsTable.appendChild(row);
+        $.each(data, function (index, university) {
+          var row = $("<tr></tr>");
+          row.append("<td>" + (index + 1) + "</td>");
+          row.append("<td>" + university.name + "</td>");
+          row.append(
+            '<td><a href="' +
+              university.web_pages[0] +
+              '">' +
+              university.web_pages[0] +
+              "</a></td>"
+          );
+          row.append("<td>" + university.country + "</td>");
+          row.append('<td><input type="checkbox" class="save-checkbox"></td>');
+          table.append(row);
         });
-      })
-      .catch((error) => {
-        alert("Произошла ошибка при выполнении запроса.");
-        console.error(error);
-      });
+
+        $("#search-results").html(table);
+        restoreCheckboxState();
+        updateCheckboxCount();
+        localStorage.setItem("universities", table.html());
+      }
+    );
   });
 
-  resetButton.addEventListener("click", function () {
-    countryInput.value = "";
-    resultsTable.innerHTML = "";
+  // Обработчик нажатия на кнопку "Сброс"
+  $("#reset-button").click(function () {
+    $("#country-input").val("");
+    $("#search-results").empty();
+    resetCheckboxState();
+    updateCheckboxCount();
+    localStorage.clear();
   });
+
+  // Обработчик изменений в чекбоксах
+  $(document).on("change", ".save-checkbox", function () {
+    saveCheckboxState();
+    updateCheckboxCount();
+  });
+
+  // Восстановление состояния чекбоксов
+  function restoreCheckboxState() {
+    $(".save-checkbox").each(function (index) {
+      var checkboxId = "checkbox-" + index;
+      var checkboxState = localStorage.getItem(checkboxId);
+      if (checkboxState === "checked") {
+        $(this).prop("checked", true);
+      } else {
+        $(this).prop("checked", false);
+      }
+      $(this).attr("id", checkboxId);
+    });
+  }
+
+  // Сохранение состояния чекбоксов
+  function saveCheckboxState() {
+    $(".save-checkbox").each(function (index) {
+      var checkboxId = "checkbox-" + index;
+      var checkboxState = $(this).prop("checked") ? "checked" : "unchecked";
+      localStorage.setItem(checkboxId, checkboxState);
+      $(this).attr("id", checkboxId);
+    });
+  }
+
+  // Сброс состояния чекбоксов
+  function resetCheckboxState() {
+    $(".save-checkbox").prop("checked", false);
+    $(".save-checkbox").each(function (index) {
+      var checkboxId = "checkbox-" + index;
+      localStorage.removeItem(checkboxId);
+      $(this).attr("id", checkboxId);
+    });
+  }
+
+  // Об  // Обновление счетчика чекбоксов
+  function updateCheckboxCount() {
+    var count = $(".save-checkbox:checked").length;
+    $("#checkbox-count").text(count);
+  }
 });
